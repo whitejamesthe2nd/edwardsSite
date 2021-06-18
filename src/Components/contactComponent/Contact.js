ca7ea57b22ea2dcee103f50c9d8249db5c3f11fd
James White <whitejamesthe2nd@gmail.com>
2021-06-18 16:06:26 -0400

    made phone number clickable

--- src/Components/contactComponent/Contact.js
+++ src/Components/contactComponent/Contact.js
@@ -5,7 +5,10 @@ let Contact = ()=>{
         <div>
             <h2>Contact Information</h2>
             <span></span><p>Edward Lamar Dixon</p>
-            <span>Phone:</span><p>419-392-0567</p>
+            <span>Phone:</span>
+            <p>
+                <a href="tel:+14193920567">  419-392-0567</a>
+            </p>
             <span>Email:</span><p></p>
             <p>Facebook Page</p>
         </div>

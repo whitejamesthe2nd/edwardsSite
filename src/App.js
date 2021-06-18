d5b1e1586f7a33f097524d5414eb59c463f67632
James White <whitejamesthe2nd@gmail.com>
2021-06-18 15:06:18 -0400

    Services Component Skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/headerComponent/header';
+import Services from './Components/servicesComponent/Services';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Services />
+
       </header>
     </div>
   );
---
+++ src/Components/servicesComponent/Services.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+
+
+
+let Services = ()=>{
+
+
+
+    return(
+        <div>
+            <h2>Services</h2>
+            <p>Tit Sucker</p>
+            <p>Clit Tugger</p>
+            <p></p>
+        </div>
+    )
+}
+
+export default Services;

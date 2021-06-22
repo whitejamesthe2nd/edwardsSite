2083bbf35d5d417eae6ae9bd871c358735ba59dd
James White <whitejamesthe2nd@gmail.com>
2021-06-22 01:23:32 -0400

    dispatch for completion

--- src/Components/EdwardsPlace/EdwardsPlaceComponent.js
+++ src/Components/EdwardsPlace/EdwardsPlaceComponent.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {useDispatch} from 'react-redux';
+import {completion} from '../../redux/actions/CompletionActions'
 
 let EdwardsPlaceComponent = ()=>{
+    const dispatch = useDispatch();
+    const [time,setTime] = useState(null);
+    const [person,setPerson] = useState(null);
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        let data = {
+            time,
+            person
+        }
+        console.log(data);
+        dispatch(completion(data))
+
+    }
+
+
     return(
         <div>
-            <h1>Hi Bro</h1>
+            <h1>Hi Bro,</h1>
             <p>This is where you will be setting your current Status</p>
+            <form onSubmit={handleSubmit}>
+
             <h3>Anticipated Time of Completion:</h3>
-            <input type='time' placeholder='Time of Completion'></input>
+            <input type='time' placeholder='Time of Completion' onChange={(e)=> setTime(e.target.value)}></input>
             <h3>Next in Line:</h3>
-            <input type='text' placeholder='next in line'></input>
+            <input type='text' placeholder='next in line' onChange={(e)=>setPerson(e.target.value)}></input>
+            <br />
+            <button type='submit'>Send</button>
+            </form>
 
         </div>
     )
